8f7a081fe2a20a2a8bdd15cbba53182306c292ee
zachj1000 <zachj1000@gmail.com>
2026-07-09 13:44:27 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'guessball-v1.5.4';
+const ASSETS = ['/guessball/', '/guessball/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // For Firebase and external requests, go straight to network
+  if (!e.request.url.includes('/guessball')) {
+    return;
+  }
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
